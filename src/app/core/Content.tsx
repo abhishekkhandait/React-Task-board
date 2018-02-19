411bdc9fb78496390977e24d1813db14be11180a
Abhishek Khandait <abhishek25khandait@gmail.com>
2018-02-20 02:09:24 +0530

    created dynamic tabs for board

--- src/app/core/Content.tsx
+++ src/app/core/Content.tsx
@@ -1,67 +1,56 @@
 
 import * as React from "react";
+import { Tab } from "material-ui";
+
 import { TasksService } from "../services/taskService";
 import { BoardModel } from "../models/boardModel";
 
-export class ContentComponent extends React.Component {
+export interface State {
+    boards: any[]
+}
+
+export class ContentComponent extends React.Component<object, State> {
 
     private taskService = new TasksService();
-    private boards: BoardModel[] = [];
+    public boards: BoardModel[];
 
     constructor(props: any) {
         super(props);
+        this.state = {
+            boards: []
+        };
         this.getBoards();
-        console.log(this.boards);
-        this.state = this.boards[0];
+
     }
 
     public getBoards() {
         this.taskService.getBoards()
             .then((res) => {
-                res.data.map((d: any) => {
-                    this.boards.push(new BoardModel(d.id, d.title, d.author));
-                })
+                this.setState({
+                    boards: res.data.map((d: any) => {
+                        return new BoardModel(d.id, d.title, d.author);
+                    }),
+                });
             })
-            .catch((err)=>  {
+            .catch((err) => {
                 console.log(`error in getting boards | message: ${err}`);
             });
     }
 
     render() {
-
         return (
-            <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
-                <div className="mdl-tabs__tab-bar" style={{ display: "flex", margin: "0 auto", justifyContent: "flex-start" }}>
-                    <a href="#starks-panel" className="mdl-tabs__tab is-active">Starks</a>
-                    <a href="#lannisters-panel" className="mdl-tabs__tab">Lannisters</a>
-                    <a href="#targaryens-panel" className="mdl-tabs__tab">Targaryens</a>
-                </div>
-
-                <div className="mdl-tabs__panel is-active" id="starks-panel">
-                    <ul>
-                        <li>Eddard</li>
-                        <li>Catelyn</li>
-                        <li>Robb</li>
-                        <li>Sansa</li>
-                        <li>Brandon</li>
-                        <li>Arya</li>
-                        <li>Rickon</li>
-                    </ul>
-                </div>
-                <div className="mdl-tabs__panel" id="lannisters-panel">
-                    <ul>
-                        <li>Tywin</li>
-                        <li>Cersei</li>
-                        <li>Jamie</li>
-                        <li>Tyrion</li>
-                    </ul>
-                </div>
-                <div className="mdl-tabs__panel" id="targaryens-panel">
-                    <ul>
-                        <li>Viserys</li>
-                        <li>Daenerys</li>
-                    </ul>
-                </div>
+            <div>
+                {
+                    this.state.boards.map((board) => {
+                        return (
+                            <Tab key={board.id} label={board.title} value={board.id}>
+                                <div>
+                                    Welcome to {board.title}
+                                </div>
+                            </Tab>
+                        )
+                    })
+                }
             </div>
         );
     }
