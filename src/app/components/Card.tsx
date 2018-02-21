0d5d5fb0d8812f6ab655c535dfec171e27be5385
Abhishek Khandait <abhishek25khandait@gmail.com>
2018-02-22 02:09:49 +0530

    changed card css and added static data for priority and duedate

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -2,16 +2,16 @@ import * as React from "react";
 
 export class CardComponent extends React.Component<any, any> {
     render() {
-        return(
+        return (
             <div className="mdl-card mdl-shadow--2dp">
-                <div className="mdl-card__title">
-                    <h4 className="mdl-card__title-text">Update</h4>
+                <div style= {{ padding: "24px 24px 0 24px" }}>
+                    <div className="card-property">Priority: 1</div>
+                    <div className="card-property">Due Date: 25/02/1993</div>
                 </div>
-                <div className="mdl-card__supporting-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aenan convallis.
+                <div className="mdl-card__supporting-text" style={{ padding: "0 24px 24px 24px", width: "auto", fontSize:"16px"}}>
+                    Lorem ipsum dolor
                 </div>
-            </div>   
+            </div>
         )
     }
 }
