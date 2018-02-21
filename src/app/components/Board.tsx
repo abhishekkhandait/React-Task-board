5b6e16c513ec83c3217567fc6620c9eebb0e1f52
Abhishek Khandait <abhishek25khandait@gmail.com>
2018-02-22 02:08:50 +0530

    added code to generate tiles dynamically

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -2,21 +2,40 @@ import * as React from "react";
 import { CardComponent } from "./Card";
 
 export class BoardComponent extends React.Component {
+
+    public tiles = [
+        { text: 'New', color: '#03A9F4' },
+        { text: 'On-Hold', color: '#f5c942' },
+        { text: 'In-Progress', color: '#d066e2' },
+        { text: 'Done', color: '#ff6b68' },
+    ];
+
     render() {
         return (
-            <div className="mdl-grid">
-                <div className="mdl-cell mdl-cell--3-col">
-                    <CardComponent />
-                </div>
-                <div className="mdl-cell mdl-cell--3-col">
-                    <CardComponent />
-                </div>
-                <div className="mdl-cell mdl-cell--3-col">
-                    <CardComponent />
-                </div>
-                <div className="mdl-cell mdl-cell--3-col">
-                    <CardComponent />
-                </div>
+            <div className="mdl-grid" style={{ position: "relative"}}>
+                {
+                    this.tiles.map((tile) => {
+                        return (
+                            <div className="mdl-cell mdl-cell--3-col">
+                                <div className="board-header">
+                                    <div className="board-header__row" style={{ backgroundColor: `${tile.color}` }}>
+                                        <span style={{ padding: "8px 0 0 4px" }}>{tile.text}</span>
+                                        <div className="mdl-layout-spacer"></div>
+                                        <button className="mdl-button mdl-button--icon">
+                                            <i className="material-icons">sort</i>
+                                        </button>
+                                        <button className="mdl-button mdl-button--icon">
+                                            <i className="material-icons">add</i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <CardComponent />
+                                <CardComponent />
+                                <CardComponent />
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
